0a9b602ae6a500ab5dcdc69fe8c5adf8c867f928
sunnypawar <sunny.s.pawar@gmail.com>
2020-12-28 16:14:36 +0530

    2804

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 // const validator = require('validator')
 
 // mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
-mongoose.connect(env.MONGODB_URL,{    
+mongoose.connect(process.env.MONGODB_URL,{    
 // mongoose.connect(secrets.MONGODB_URL,{  
     useNewUrlParser:true,
     useCreateIndex:true,
