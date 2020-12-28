af8a6d591c3d2cc253f19c1fad56d06f5c7745f0
sunnypawar <sunny.s.pawar@gmail.com>
2020-12-29 03:47:59 +0530

    2819

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const Task = require('./task')
+const envs = require('../env');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -81,7 +82,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id: user._id.toString()} , process.env.JWT_SECRET)
+    const token = jwt.sign({_id: user._id.toString()} , JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
